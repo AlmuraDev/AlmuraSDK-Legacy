5827b604d10b57de9ae296e1e4963793bf813483
Steven Downer <grinch@outlook.com>
2015-09-02 17:52:34 -0500

    Update build.gradle
    Signed-off-by: Steven Downer <grinch@outlook.com>

--- src/main/java/com/almuradev/almurasdk/util/FileSizeUtil.java
+++ src/main/java/com/almuradev/almurasdk/util/FileSizeUtil.java
@@ -1,3 +1,27 @@
+/*
+ * This file is part of AlmuraSDK, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) AlmuraDev <http://github.com/AlmuraDev/AlmuraSDK/>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.almuradev.almurasdk.util;
 
 public class FileSizeUtil {
@@ -8,7 +32,7 @@ public class FileSizeUtil {
         if (value < 0) return "-" + format(-value);
         if (value == 0) return "0B";
 
-        final int mag = (int) (Math.log10(value) / Math.log10(1024));
+        final int mag = (int) (Math.log10(value) / Math.log10(1000));
         final float adjustedSize = (float) value / (1L << (mag * 10));
 
         final String format = (mag > 2 ? "%.2f" : "%.0f") + "%s";
